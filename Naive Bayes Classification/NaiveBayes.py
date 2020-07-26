da285a22846988cdc5ba3a2da910e6eb8da93357
KritikPant <kritik1411@outlook.com>
2020-07-26 13:08:37 +0100

    Naive Bayes algorithm

--- Naive Bayes Classification/NaiveBayes.py
+++ Naive Bayes Classification/NaiveBayes.py
@@ -13,11 +13,11 @@ class NaiveBayes:
 		self._var = np.zeros((n_classes, n_features), dtype = np.float64)	
 		self._priors = np.zeros(n_classes, dtype = np.float64)
 
-		for c in self._classes:
-			X_c = X[c==y]
-			self._mean[c, :] = X_c.mean(axis=0)
-			self._mean[c, :] = X_c.var(axis=0)
-			self._priors[c] = X_c.shape[0]/float(n_samples) #Prior probability = frequency in training data
+		for idx, c in enumerate(self._classes):
+			X_c = X[y==c]
+			self._mean[idx, :] = X_c.mean(axis=0)
+			self._var[idx, :] = X_c.var(axis=0)
+			self._priors[idx] = X_c.shape[0] / float(n_samples) #Prior probability = frequency in training data
 
 	def predict(self, X):
 		y_pred = [self._predict(x) for x in X]
@@ -29,8 +29,8 @@ class NaiveBayes:
 
 		for index, c in enumerate(self._classes):
 			prior = np.log(self._priors[index])
-			class_conditional = np.sum(np.log(self._pdf[index, x]))
-			posterior = prior + class_conditional
+			posterior = np.sum(np.log(self._pdf(index, x)))
+			posterior = prior + posterior
 			posteriors.append(posterior)
 		
 		return self._classes[np.argmax(posteriors)]
@@ -38,6 +38,6 @@ class NaiveBayes:
 	def _pdf(self, class_index, x): #Probability density function
 		mean = self._mean[class_index]
 		var = self._var[class_index]
-		numerator = np.exp(- (x - mean ** 2 / (2 * var)))
+		numerator = np.exp(- ((x - mean) ** 2 / (2 * var)))
 		denominator = np.sqrt(2 * np.pi * var)
 		return numerator/denominator
